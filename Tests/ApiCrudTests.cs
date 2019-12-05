e60c83797090eeadb6dcdbbfa74973ed68080aff
jrocha <jrocha@usbr.gov>
2019-12-05 14:59:28 -0700

    comment out changes made for 153192828888e25de7369583d9cded47f61888d2. need to keep working on this code and get it working...

--- Tests/ApiCrudTests.cs
+++ Tests/ApiCrudTests.cs
@@ -26,7 +26,7 @@ namespace HdbApi.Tests
         public void SeriesRtableInsertEndPoint()
         {
             var hdbProcessor = new HdbApi.App_Code.HdbCommands();
-            var result = hdbProcessor.modify_r_base_raw(conx, testSDI, testInterval, testT, testVal, testBool, testFlag, testBool, testLoadingApplicationId);
+            var result = hdbProcessor.modify_r_base_raw(conx, testSDI, testInterval, testT, testVal, testBool, testFlag, testBool);//, testLoadingApplicationId);
 
             var seriesProcessor = new DataAccessLayer.SeriesRepository();
             var verification = seriesProcessor.GetSeries(conx, testSDI, testInterval, testT, testT);
@@ -39,7 +39,7 @@ namespace HdbApi.Tests
         public void SeriesRtableDeleteEndPoint()
         {
             var hdbProcessor = new HdbApi.App_Code.HdbCommands();
-            var result = hdbProcessor.modify_r_base_raw(conx, testSDI, testInterval, testT, testVal, testBool, testFlag, testBool, testLoadingApplicationId);
+            var result = hdbProcessor.modify_r_base_raw(conx, testSDI, testInterval, testT, testVal, testBool, testFlag, testBool);//, testLoadingApplicationId);
             hdbProcessor.delete_from_hdb(conx, testSDI, testT, testInterval);
 
             var seriesProcessor = new DataAccessLayer.SeriesRepository();
