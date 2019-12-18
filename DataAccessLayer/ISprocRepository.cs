bae133ba40388cdb60d66bad504b9decf16cd58a
jrocha <jrocha@usbr.gov>
2019-12-18 16:37:28 -0700

    Working and tested commit for #3. Waiting on user acceptance and further testing.

--- DataAccessLayer/ISprocRepository.cs
+++ DataAccessLayer/ISprocRepository.cs
@@ -44,9 +44,27 @@ namespace HdbApi.DataAccessLayer
             p.Add("@P_SITE_DATATYPE_ID", 2101, dbType: OracleDbType.Decimal);
             var funcCommand = new CommandDefinition("HDB_UTILITIES.IS_SDI_IN_ACL", p, commandType: CommandType.StoredProcedure);
             var funcResult = (List<dynamic>)db.Query<dynamic>(funcCommand);
-            
+
+            //test passing in sql command
+            var sqlString = "select * from hdb_site";
+            var sqlStringResult = (List<dynamic>)db.Query<dynamic>(sqlString, commandType: CommandType.Text);
 
             return new List<dynamic>();
+        }
+
+
+        public List<dynamic> RunSqlSelect(IDbConnection db, string sqlStatement)
+        {
+            var badStrings = new[] { "UPDATE", "DELETE", "INSERT", "CREATE", "ALTER", "DROP" };
+            if (badStrings.Any(sqlStatement.ToUpper().Contains))
+            {
+                throw new Exception("Users may only run select statements using this API end-point. ");
+            }
+            else
+            {
+                var sqlStringResult = (List<dynamic>)db.Query<dynamic>(sqlStatement, commandType: CommandType.Text);
+                return sqlStringResult;
+            }            
         }
 
     }
